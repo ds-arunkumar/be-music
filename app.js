b0d05f03be598657fdb8b8f60fcade03f28f44dd
ds-arunkumar <arunkumar519@gmail.com>
2025-04-15 11:01:09 +0530

    netlify link added

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 // enable cors
 app.use(
   cors({
-    origin: "https://exquisite-druid-103215.netlify.app/",
+    origin: "https://exquisite-druid-103215.netlify.app",
     credentials: true,
   })
 );
